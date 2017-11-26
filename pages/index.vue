488cb4db963df38d99833414ded5b848267f0def
Raymon S <raymon@raymonschouwenaar.nl>
2017-11-26 17:09:01 +0100

    New structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,64 @@
 <template>
   <section class="container">
-    <h1 class="title">
-      Mr Frontend Blog
-    </h1>
-    <ul v-if="posts && posts.length">
-      <li v-for="post of posts">
-        <p><h2 v-html="post.title.rendered"></h2></p>
-        <img :src="post.better_featured_image.media_details.sizes.medium.source_url" />
-        <p v-html="post.excerpt.rendered"></p>
-        <hr>
-      </li>
-    </ul>
-
-    <ul v-if="errors && errors.length">
-      <li v-for="error of errors">
-        {{error.message}}
-      </li>
-    </ul>
-    <nuxt-link class="button" to="/about">
-      About page
-    </nuxt-link>
+    <div>
+      <logo/>
+      <h1 class="title">
+        Mr Frontend Blog
+      </h1>
+      <div class="links">
+        <ul>
+          <li v-for="post in posts">
+            <nuxt-link :to="{ name: 'posts-id', params: { id: post.id } }"><span v-html="post.title.rendered"></span></nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
 import axios from 'axios'
+import Logo from '~/components/Logo.vue'
 
 export default {
-  data() {
-    return {
-      posts: [],
-      errors: []
-    }
+  async asyncData ({ params }) {
+    // We can use async/await ES6 feature
+    let { data } = await axios.get(`https://blog.mrfrontend.org/wp-json/wp/v2/posts/`)
+    return { posts: data }
   },
-  created() {
-    axios.get('https://blog.mrfrontend.org/wp-json/wp/v2/posts/')
-      .then(posts => {
-          // console.log('this: ', this);
-          this.posts = posts.data;
-      })
-      .catch(e => {
-        this.errors.push(e)
-      });
-    }
+  components: {
+    Logo
+  }
 }
 </script>
 
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
 
-<style scoped>
-.title
-{
-  margin: 50px 0;
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
 }
 </style>
