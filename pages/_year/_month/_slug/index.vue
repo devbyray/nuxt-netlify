940ba4effe306cd6a44866ce4fd83d5bb39c52a0
Raymon S <raymon@raymonschouwenaar.nl>
2017-12-12 00:01:53 +0100

    Add new routing

---
+++ pages/_year/_month/_slug/index.vue
@@ -0,0 +1,197 @@
+<template>
+  <article class="mfd__post">
+    <div class="mfd__post-title">
+      <strong>lkashdlkasd</strong>
+      <h1 class="title" v-html="post.title.rendered"></h1>
+    </div>
+    <div class="mfd__post-image" v-if="post.better_featured_image !== undefined && post.acf.youtube_url === undefined">
+      <img :src="post.better_featured_image.source_url" :alt="post.title.rendered">
+    </div>
+    <div class="mfd__post-video" v-if="post.acf.youtube_url !== undefined">
+      <div class="embed-youtube">
+        <iframe id="ytplayer" type="text/html" width="640" height="360" :src="getYoutubeEmbedUrl(post.acf.youtube_url)" frameborder="0"></iframe>
+      </div>
+    </div>
+    <div class="mfd__post-date">
+      <strong>Published: </strong> <em :title="post.date">{{moment(post.date).fromNow()}}</em>
+    </div>
+    <div class="mfd__post-content">
+      <div class="content" v-html="post.content.rendered"></div>
+    </div>
+    <div class="mfd__post-comments">
+      <h2>Comments</h2>
+      <disqus shortname="rsschouwenaar" :identifier="post.id"></disqus>
+    </div>
+  </article>
+</template>
+
+<script>
+/* Slug page */
+import axios from 'axios'
+import moment from 'moment'
+import Sidebar from '~/components/Sidebar.vue'
+import VueDisqus from 'vue-disqus/VueDisqus.vue'
+
+const getYoutubeEmbedUrl = (youtubeUrl) => {
+  let yt = 'https://www.youtube.com/watch?v=hFi0hUnvr0s'
+  let searchId = yt.indexOf('?v=')
+  let VIDEO_ID = yt.substring(searchId + 3)
+
+  return `https://www.youtube.com/embed/${VIDEO_ID}`
+}
+
+// const filterYTandSoundcloud = () => {
+//   let ytPlayerElement = document.querySelector('.youtube-player')
+//   let ytPlayerSpanParent = ytPlayerElement.parentNode
+//   let ytPlayerParagraph = ytPlayerSpanParent.parentNode
+//   ytPlayerParagraph.outerHTML = ''
+// }
+
+// filterYTandSoundcloud();
+
+export default {
+  layout: 'post',
+  async asyncData ({ params }) {
+    // We can use async/await ES6 feature
+    let { data } = await axios.get(
+      `https://blog.mrfrontend.org/wp-json/wp/v2/posts?slug=${params.slug}`
+    )
+
+    return {
+      post: data[0],
+      permalink: `${params.year}/${params.month}/${params.slug}`
+    }
+  },
+  data () {
+    return {
+      moment,
+      getYoutubeEmbedUrl
+    }
+  },
+  components: {
+    Sidebar,
+    VueDisqus
+  },
+  computed: {
+    disqusShortname () {
+      return 'rsschouwenaar'
+    },
+    disqusId () { // env used to avoid re-use from dev to production
+      return `${process.env.NODE_ENV}-${this.disqusShortname}-${this.post.id}`
+    }
+  },
+  head () {
+    let post = this.post
+    return {
+      title: unescape(post.title.rendered)
+      // meta: [
+      //   {
+      //     hid: post.meta.id,
+      //     name: post.meta.name,
+      //     content: post.meta.content
+      //   }
+      // ]
+    }
+  },
+  watch: {
+    '$route.params.slug' (curr, old) {
+      // disqus does not properly reload just based off the
+      // disqusId computed property - we need to manually change it
+      // when we know it should update
+      this.$refs.disqus.init()
+    }
+  }
+
+}
+</script>
+
+<style>
+@supports (display: grid) {
+  .mfd__post {
+    display: grid;
+    grid-gap: 2em;
+    grid-template-rows: 1fr;
+    grid-template-columns: repeat(12,1fr);
+  }
+  .mfd__post-title {
+    grid-row: 1;
+    grid-column: 2/12
+  }
+  .mfd__post-image,
+  .mfd__post-video {
+    grid-row: 2;
+    grid-column: 2/12
+  }
+  .mfd__post-date {
+    grid-row: 3;
+    grid-column: 2/12
+  }
+  .mfd__post-content {
+    grid-row: 4;
+    grid-column: 2/12
+  }
+  .mfd__post-comments {
+    grid-row: 5;
+    grid-column: 2/12
+  }
+
+  @media screen
+    and (min-width: 900px) {
+      .mfd__post-title {
+        grid-column: 3/11;
+        text-align: justify;
+      }
+      .mfd__post-image,
+      .mfd__post-video {
+        grid-column: 2/12;
+      }
+      .mfd__post-date {
+        grid-column: 3/4
+      }
+      .mfd__post-content {
+        grid-row: 3;
+        grid-column: 5/11
+      }
+      .mfd__post-comments {
+        grid-row: 4;
+        grid-column: 5/11
+      }
+
+  }
+}
+@media screen
+  and (min-width: 900px) {
+  .mfd__post-title .title {
+    font-size: 4rem;
+  }
+}
+@media screen
+  and (min-width: 1600px) {
+  .mfd__post-title .title {
+    font-size: 4rem;
+  }
+}
+.mfd__post-title .title {
+  font-size: 2rem;
+}
+
+.mfd__post-content iframe {
+  max-width: 100%;
+}
+.mfd__post-content p,
+.mfd__post-content ol,
+.mfd__post-content ul {
+  margin-bottom: 1.6rem;
+}
+.mfd__post-content code {
+  display: inline;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+  font-size: .8rem;
+  font-weight: 800;
+}
+.mfd__post-content pre {
+    font-size: .8rem;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="mfd__blog-list">
     <div v-for="post in posts" class="mfd__blog-item" :key="post.id">
         <div class="mfd-post__link">
-          <nuxt-link class="mfd__blog-link" :to="{ name: 'slug', params: { slug: post.slug }}">
+          <nuxt-link class="mfd__blog-link" :to="{ name: 'year-month-slug', params: { slug: post.slug, year: getYear(post.date), month: getMonth(post.data) }}">
             <h2 class="mfd-post__link-title" v-html="post.title.rendered"></h2>
           </nuxt-link>
         </div>
@@ -12,7 +12,7 @@
         <p class="mfd-post__date"><strong>Published: </strong> <em :title="post.date">{{moment(post.date).fromNow()}}</em></p>
         <span class="mfd-post__excerpt" v-html="post.excerpt.rendered"></span>
         <p class="mfd-post__readmore">
-          <nuxt-link class="mfd-post__readmore-link" :to="{ name: 'slug', params: { slug: post.slug }}">Read more ></nuxt-link>
+          <nuxt-link class="mfd-post__readmore-link" :to="{ name: 'year-month-slug', params: { slug: post.slug, year: getYear(post.date), month: getMonth(post.data) }}">Read more ></nuxt-link>
         </p>
     </div>
   </div>
@@ -21,20 +21,26 @@
 import axios from 'axios'
 import moment from 'moment'
 
+const getYear = (dateInput) => {
+  return moment(dateInput).format('YYYY')
+}
+const getMonth = (dateInput) => {
+  return moment(dateInput).format('MM')
+}
+
 export default {
   async asyncData ({ params }) {
     // We can use async/await ES6 feature
     let { data } = await axios.get(
       `https://blog.mrfrontend.org/wp-json/wp/v2/posts/`
     )
-
-    console.log('data: ', data[0])
-
     return { posts: data }
   },
   data () {
     return {
-      moment: moment
+      moment,
+      getYear,
+      getMonth
     }
   }
 }
